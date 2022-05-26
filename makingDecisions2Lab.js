c8e4d0f9584aaa0fbc5b569f2b67e4a4eb5ea2bc
Tyler Kendall <tylerchristiankendall@gmail.com>
2022-05-26 14:34:35 -0600

    Basic Problems Completed

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -6,15 +6,17 @@ var arr = [10,20,30,40,50,60]
   Create a variable named 'firstItem' and set it equal to the first value of the 'arr' array.
 */
 
-//Code Here
-
+let firstItem = arr[0];
+console.log(firstItem);
 
 ////////// PROBLEM 2 //////////
 /*
   Remove the last item from the 'arr' array and store it in a variable named 'lastItemRemoved'. Console.log the array to verify you no longer have the last item.
 */
 
-//Code Here
+let lastItemRemoved = arr.pop();
+console.log(lastItemRemoved);
+console.log(arr);
 
 ////////// PROBLEM 3 //////////
 
@@ -26,9 +28,10 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
   Loop through the 'family' array and console.log every item.
 */
 
-//Code Here
-
-
+for(let i = 0; i < family.length; i++)
+{
+  console.log(family[i]);
+}
 
 ////////// PROBLEM 4 //////////
 
@@ -41,21 +44,50 @@ let evensArr = []
   Use a for-loop to iterate through each number in the 'nums' array, if the number is even, append it to the 'evensArr'.
 */
 
-//Code Here
-
-
+for (let i = 0; i < nums.length; i++)
+{
+  if (nums[i] % 2 === 0)
+  {
+    evensArr.push(nums[i]);
+  }
+}
+console.log(evensArr);
 
 ////////// PROBLEM 5 //////////
 
 // Do not edit the code below.
-var score = 74
+let score = 90
 // Do not edit the code above.
 
 /*
   Determine if the letter grade of the given variable 'score'. If the variable is a 90 or above, console log an 'A', between 80 and 89, console log a 'B', between 70 and 79, 'C', between 60 and 69, 'D', and anything below 60 should console log an 'F'.
 */
 
-//Code Here
+if (score >= 90)
+{
+  console.log('A');
+}
+else if (score >= 80)
+{
+  console.log('B');
+}
+else if (score >= 70)
+{
+  console.log('C');
+}
+else if (score >= 60)
+{
+  console.log('D');
+} 
+else if (score >= 0)
+{
+  console.log('F');
+}
+else
+{
+  console.log('Error, number is below 0');
+}
+
 
 
 ////////// Intermediate Problems //////////
